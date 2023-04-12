13b1a7f84a00a467466e673fe29af9a08b66f192
redghost <94316140+R3D-GHOST@users.noreply.github.com>
2023-04-12 17:37:39 +0000

    Add files via upload

---
+++ install.py
@@ -0,0 +1,98 @@
+#Librerias
+import os
+import time
+#Colores
+negro = '\033[30m'
+rojo = '\033[31m'
+verde = '\033[32m'
+amarillo = '\033[33m'
+azul = '\033[34m'
+rosado = '\033[35m'
+calipso= '\033[36m'
+blanco = '\033[37m'
+cierre = '\033[39m'
+#banner
+banner = """
+ _                _                 __        ____  __ 
+| |    __ _ _ __ | |_ ___  _ __     \ \      / /  \/  |
+| |   / _` | '_ \| __/ _ \| '_ \ ____\ \ /\ / /| |\/| |
+| |__| (_| | |_) | || (_) | |_) |_____\ V  V / | |  | |
+|_____\__,_| .__/ \__\___/| .__/       \_/\_/  |_|  |_|
+           |_|            |_|                          
+
+"""
+#Varibles
+def req():
+    os.system('clear')
+    os.system("sudo apt-get update -y")
+    os.system("sudo apt install net-tools libuv1-dev build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev -y")
+    os.system("sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y")
+    os.system("sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y")
+    os.system("sudo apt install bspwm rofi caja feh gnome-terminal scrot neovim xclip tmux acpi scrub bat wmname -y")
+   
+    print("[+] Instalacion de BSPWM Finalizada")
+    
+def poly():
+    os.system("git clone --recursive https://github.com/polybar/polybar")
+    os.system("mv polybar/* .")
+    os.system("sudo rm -r polybar/")
+    os.system("cmake .")
+    os.system("make -j$(nproc)")
+    os.system("sudo make install")
+    os.system("sudo rm -r bin/ cmake/ CMakeFiles/ common/ config/ contrib/ doc/ generated-sources/ include/ lib/ libs/ polybar/ src/ tests/ banner.png build.sh CHANGELOG.md CMajeCache.txt cmake_install.cmake CMakeLists.txt compile_commands.json CONTRIBUTING.md install_manifest LICENSE Makefile README.md SUPPORT.md version.txt")
+    os.system("git clone https://github.com/ibhagwan/picom.git")
+    os.system("mv picom/* .")
+    os.system("sudo rm -r picom/")
+    os.system("git submodule update --init --recursive")
+    os.system("meson --buildtype=release . build")
+    os.system("ninja -C build")
+    os.system("sudo ninja -C build install")
+    os.system("sudo rm -r *.md *.conf *.desktop *.txt *.build *.spdx *.glsl COPYING Doxyfile CONTRIBUTORS bin/ build/ dbus-examples/ LICENSES/ man/ media/ meson/ src/ subprojects/ tests/")
+    os.system("cp -r .config /home/$USER/")
+    os.system("sudo apt install polybar ; sudo apt install picom")
+    print("[+] Instalacion de Requerimientos Finalizada")
+
+def bspwm():
+    os.system("git clone https://github.com/baskerville/bspwm.git")
+    os.system("mv bspwm/* .")
+    os.system("sudo rm -r bspwm/")
+    os.system("make")
+    os.system("sudo make install")
+    os.system("sudo rm -r artworks/ contrib/ doc/ src/ tests/ bspc bspc.o bspwm bspwm.o desktop.o events.o ewmh.o geometry.o helpers.o history.o jsmn.o LICENSE Makefile messages.o monitor.o parse.o pointer.o query.o README.md restore.o rule.o settings.o Sourcedeps stack.o subscribe.o tree.o VERSION window.o")
+    os.system("git clone https://github.com/baskerville/sxhkd.git")
+    os.system("mv sxhkd/* .")
+    os.system("sudo rm -r sxhkd/")
+    os.system("cd ../sxhkd")
+    os.system("make")
+    os.system("sudo make install")
+    os.system("mkdir ~/.config/bspwm")
+    os.system("mkdir ~/.config/sxhkd")
+    os.system("cp examples/bspwmrc ~/.config/bspwm/")
+    os.system("chmod +x ~/.config/bspwm/bspwmrc")
+    os.system("cp examples/sxhkdrc ~/.config/sxhkd/")
+    os.system("sudo rm -r contrib/ doc/ examples/ src/ grab.o helpers.o LICENSE Makefile parse.o README.md Sourcedeps sxhkd sxhkd.o types.o VERSION")
+    os.system("sudo apt install kitty ; sudo apt install zsh ")
+    print("[+] Instalacion Polybar Rofi Picom... Finalizada")
+#menu
+os.system('clear')
+print(azul+banner)
+print(amarillo+"1 --> Instalar Requerimientos")
+print(amarillo+"2 --> Instalar Bspwm Sxhkd")
+print(amarillo+"3 --> Instalar Polybar Picom Rofi ....")
+print(amarillo+"4 --> Instalar Todo")
+print(rojo+"5 --> Salir ")
+opt_menu = input(">>>> ")
+if opt_menu == "1":
+    req()
+if opt_menu == "2":
+    bspwm()
+if opt_menu == "3":
+    poly()
+if opt_menu == "4":
+    req()
+    bspwm()
+    poly()
+if opt_menu == "5":
+    exit
+else:
+    print("Error Opcion No Valida")
